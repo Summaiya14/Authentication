c05a31c265a7ccb5a67338b17c60fd0d978bdb6b
summaiya14 <summaiyamahmod@gmail.com>
2020-04-20 09:58:56 +0500

    minor changes

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import { View, TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common/Index';
+import { Button, Card, CardSection, Input } from './common/Index';
 
 class LoginForm extends Component{
+state = { email: '', password: '' };
 	render(){
 		return <Card>
 		<CardSection>
-		<TextInput style={{ height: 20, width: 100 }}/>
+		<Input
+		placeholder='user@gmail.com'
+		label='Email'
+		value={this.state.email}
+		onChangeText={text => this.setState({email: text})}
+		/>
 		</CardSection>
 
 		<CardSection>
+		<Input
+		secureTextEntry
+		placeholder='password'
+		label='Password'
+		value={this.state.password}
+		onChangeText={text => this.setState({password: text})}
+		/>
 		</CardSection>
 
 		<CardSection>
